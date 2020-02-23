ec53d8a0dd970b950766668755706696a73baca2
d1rty <d1rtyst4r@gmail.com>
2020-02-23 08:59:08 +0200

    lesson 6

---
+++ Lesson3/Stock/Stock.java
@@ -0,0 +1,30 @@
+package lv.javaguru.JavaGuruQA2.Lesson3.Book.Stock;
+
+class Stock {
+
+    String name;
+    double minPrice;
+    double maxPrice;
+    double price;
+
+    public Stock(String name, double price) {
+        this.name = name;
+        this.price = price;
+        minPrice = price;
+        maxPrice = price;
+    }
+
+    void printInformation() {
+        System.out.println("Company = " + name + ", Current price = " + price + ", Max price = " + maxPrice +
+                ", Min price = " + minPrice);
+    }
+
+    void updatePrice(double newPrice) {
+        price = newPrice;
+        if (newPrice < minPrice) {
+            minPrice = newPrice;
+        } else if (newPrice > maxPrice) {
+            maxPrice = newPrice;
+        }
+    }
+}
---
+++ Lesson3/Stock/StockTest.java
@@ -0,0 +1,17 @@
+package lv.javaguru.JavaGuruQA2.Lesson3.Book.Stock;
+
+class StockTest {
+
+    public static void main(String[] args) {
+        Stock stock = new Stock("IBM", 100);
+        stock.printInformation();
+        double newPrice = 130;
+        stock.updatePrice(newPrice);
+        stock.printInformation();
+        stock.updatePrice(80);
+        stock.updatePrice(140);
+        stock.updatePrice(120);
+        stock.printInformation();
+    }
+}
+
