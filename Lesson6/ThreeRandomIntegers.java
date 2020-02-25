f8f8d0faba5c23b354bf8a96f84f6bc570049f45
d1rty <d1rtyst4r@gmail.com>
2020-02-25 20:27:28 +0200

    Three dices simulation.

--- Lesson6/ThreeRandomIntegers.java
+++ Lesson6/ThreeRandomIntegers.java
@@ -5,8 +5,11 @@ import java.util.Random;
 public class ThreeRandomIntegers {
     public static void main(String[] args) {
         Random randomGenerator = new Random();
-        for (int i = 0; i < 3; i++){
-           System.out.println(randomGenerator.nextInt(100));
+        int numberOnFirstDice = randomGenerator.nextInt(6);
+        int numberOnSecondDice = randomGenerator.nextInt(6);
+        int numberOnThirdDice = randomGenerator.nextInt(6);
+        if (numberOnFirstDice == numberOnSecondDice && numberOnSecondDice == numberOnThirdDice){
+            System.out.println("You have Win!!! you have " + numberOnFirstDice + "on all dices.");
         }
     }
 }
